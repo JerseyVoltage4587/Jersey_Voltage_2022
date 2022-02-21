2b934ea5f481fcf7ea0b12125cb5ab9ddf22a919
Team4587 <info@jerseyvoltage.com>
2022-02-21 13:42:41 -0600

    sam's commit (fixed auto command choice system)

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -8,7 +8,7 @@ import edu.wpi.first.wpilibj.GenericHID;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.Command;
 
-import frc.robot.commands.AutoChoice;
+import frc.robot.util.AutoChoice;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
---
+++ src/main/java/frc/robot/util/AutoChoice.java
@@ -0,0 +1,39 @@
+// Copyright (c) FIRST and other WPILib contributors.
+// Open Source Software; you can modify and/or share it under the terms of
+// the WPILib BSD license file in the root directory of this project.
+
+package frc.robot.util;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Scanner;
+
+import edu.wpi.first.wpilibj2.command.Command;
+import frc.robot.commands.TwoBallAuto;
+
+public class AutoChoice {
+  /** Creates a new Auto. */
+    static File file = (new File("frc.robot.AutoCommandChoice"));
+    static String autoChoice;
+    public static HashMap<String, Command> autoCommands = new HashMap<String, Command>();
+
+  public AutoChoice() {
+    // Use addRequirements() here to declare subsystem dependencies.
+    autoCommands.put("TwoBallAuto", new TwoBallAuto());
+  }
+
+  public static Command getAutoChoice() {
+    try {
+        Scanner r = new Scanner(file);
+        autoChoice = r.next();
+        r.close();
+    }
+  
+    catch (FileNotFoundException e) {
+    System.out.println(e);
+    }
+
+    return autoCommands.get(autoChoice);
+  }
+}
