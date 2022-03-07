08afec5e344d48afc1fddc93fbeff67f623fc37d
cookie-monster- <ilive4legos+github@gmail.com>
2022-03-07 17:56:03 -0600

    commit

--- src/main/java/frc/robot/subsystems/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber.java
@@ -6,7 +6,10 @@ package frc.robot.subsystems;
 
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
+import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import frc.robot.Constants;
 
 public class Climber extends SubsystemBase {
   /** Creates a new Climber. */
@@ -14,10 +17,16 @@ public class Climber extends SubsystemBase {
   static Shooter m_Instance = null;
   private static WPI_TalonSRX m_leftClimberMotor = null;
   private static WPI_TalonSRX m_rightClimberMotor = null;
-  public Climber() {
+  private Solenoid m_leftClimberSolenoid;
+  private Solenoid m_rightClimberSolenoid;
+  private int numRotations;
+  public Climber(int rotations) {
     if (m_isActive == false) {
       return;
     }
+    numRotations = rotations;
+    m_leftClimberSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, Constants.LeftClimberChannel);
+    m_rightClimberSolenoid = new Solenoid(PneumaticsModuleType.CTREPCM, Constants.RightClimberChannel);
   }
 
   public static Shooter getInstance() {
@@ -27,6 +36,28 @@ public class Climber extends SubsystemBase {
 			}
 		}
 		return m_Instance;
+  }
+
+  //Unspool winches by set amount
+  public void climbStep1() {
+    
+  }
+
+  //Deploy pistons to lock
+  public void climbStep2() {
+    m_leftClimberSolenoid.set(true);
+    m_rightClimberSolenoid.set(true);
+  }
+
+  //Reel in the winches
+  public void climbStep3() {
+    
+  }
+
+  //Retract pistons
+  public void climbStep4() {
+    m_leftClimberSolenoid.set(false);
+    m_rightClimberSolenoid.set(false);
   }
 
   @Override
