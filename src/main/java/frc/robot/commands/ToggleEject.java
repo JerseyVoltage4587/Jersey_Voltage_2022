db852aac12e1e242f9e7ee781cef8631da09a093
Team4587 <info@jerseyvoltage.com>
2022-03-08 17:03:10 -0600

    sam's commit

--- src/main/java/frc/robot/commands/ToggleEject.java
+++ src/main/java/frc/robot/commands/ToggleEject.java
@@ -5,6 +5,7 @@
 package frc.robot.commands;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
+import edu.wpi.first.wpilibj2.command.CommandScheduler;
 import frc.robot.Robot;
 
 public class ToggleEject extends CommandBase {
@@ -17,7 +18,8 @@ public class ToggleEject extends CommandBase {
   // Called when the command is initially scheduled.
   @Override
   public void initialize() {
-    Robot.getIntake().ToggleIntakeBackward();
+    CommandScheduler.getInstance().schedule(new EjectBall());
+    //Robot.getIntake().ToggleIntakeBackward();
   }
 
   // Called every time the scheduler runs while the command is scheduled.
@@ -26,7 +28,9 @@ public class ToggleEject extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {}
+  public void end(boolean interrupted) {
+    CommandScheduler.getInstance().schedule(new StopEjectBall());
+  }
 
   // Returns true when the command should end.
   @Override
