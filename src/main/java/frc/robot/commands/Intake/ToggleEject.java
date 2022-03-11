62f7b8dea4c1c678e9f1d983d0e91206faad4f85
cookie-monster- <ilive4legos+github@gmail.com>
2022-03-10 20:49:37 -0600

    kasim's commit

--- src/main/java/frc/robot/commands/Intake/ToggleEject.java
+++ src/main/java/frc/robot/commands/Intake/ToggleEject.java
@@ -28,9 +28,7 @@ public class ToggleEject extends CommandBase {
 
   // Called once the command ends or is interrupted.
   @Override
-  public void end(boolean interrupted) {
-    CommandScheduler.getInstance().schedule(new StopEjectBall());
-  }
+  public void end(boolean interrupted) {}
 
   // Returns true when the command should end.
   @Override
--- src/main/java/frc/robot/commands/Intake/ToggleIntake.java
+++ src/main/java/frc/robot/commands/Intake/ToggleIntake.java
@@ -17,9 +17,7 @@ public class ToggleIntake extends CommandBase {
 
   // Called when the command is initially scheduled.
   @Override
-  public void initialize() {
-    CommandScheduler.getInstance().schedule(new IntakeBall());
-  }
+  public void initialize() {}
 
   // Called every time the scheduler runs while the command is scheduled.
   @Override
